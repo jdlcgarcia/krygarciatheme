84dae59428e1a687d291e7f21ff16f1f477538a6
Juan Garcia <jgarcia@qashops.com>
2017-12-13 17:19:49 +0100

    widgetized sidebar

--- functions.php
+++ functions.php
@@ -9,14 +9,27 @@ function load_scripts()
 
 add_action('wp_enqueue_scripts', 'load_scripts');
 
-function register_my_menus() {
-  register_nav_menu('header-menu',__( 'Header Menu' ));
-  register_nav_menu('social-menu',__( 'Social Menu' ));
+function register_my_menus()
+{
+    register_nav_menu('header-menu', __('Header Menu'));
+    register_nav_menu('social-menu', __('Social Menu'));
 }
-add_action( 'init', 'register_my_menus' );
-
+add_action('init', 'register_my_menus');
+add_action('widgets_init', 'sidebar_widgets_init');
 
 function adjustBrightness($hex, $steps)
 {
-    
+}
+
+function sidebar_widgets_init()
+{
+
+    register_sidebar(array(
+        'name'          => 'Blog right sidebar',
+        'id'            => 'blog_right',
+        'before_widget' => '<div class="widget">',
+        'after_widget'  => '</div>',
+        'before_title'  => false,
+        'after_title'   => false,
+    ));
 }
--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php get_header(); ?>
-    <section class="content">
-        
-    <?php
-    if (false == have_posts()) {
-    } else {
-        while (have_posts()) {
-            the_post();
-            ?>
-            <article>
-                <h1><?php the_title(); ?></h1>
-                <span class="subtitle"><?php the_time('Y.m.D') ?></span>
-                <?php the_post_thumbnail(); ?>
-                <p class="postContent"><?php echo get_the_excerpt(); ?></p>
-                <a class="readMore" href="<?php the_permalink(); ?>">Read more</a>
-            </article>
-            <?php
+    <div class="container">
+        <section class="content">
+            
+        <?php
+        if (false == have_posts()) {
+        } else {
+            while (have_posts()) {
+                the_post();
+                ?>
+                <article>
+                    <h1><?php the_title(); ?></h1>
+                    <span class="subtitle"><?php the_time('Y.m.D') ?></span>
+                    <?php the_post_thumbnail(); ?>
+                    <p class="postContent"><?php echo get_the_excerpt(); ?></p>
+                    <a class="readMore" href="<?php the_permalink(); ?>">Read more</a>
+                </article>
+                <?php
+            }
         }
-    }
-    ?>       
-    </section>
+        ?>       
+        </section>
+
+        <?php get_sidebar(); ?>
+    </div>
 <?php get_footer(); ?>
